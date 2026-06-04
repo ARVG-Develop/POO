891fbd460a0b18175587ad99dfbad490d93886ec
AlessandroFTunes <alesfernandes123@gmail.com>
2026-06-03 21:19:53 -0300

    tralal

--- trabalhopratico/obj/Debug/net8.0/SistemaBiblioteca.AssemblyInfo.cs
+++ trabalhopratico/obj/Debug/net8.0/SistemaBiblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaBiblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e58b5427d660f8d779f6f117e020f3f90ba4cca5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21a0b334fa98331c5832d63b8a725d50ca4f6543")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaBiblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaBiblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
